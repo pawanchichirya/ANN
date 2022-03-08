df884780e835e49d7d286432417c8bdd42b48478
DeploymentsPractice <deploymentspractice@gmail.com>
2022-03-08 19:56:27 +0530

    "early stopping"

--- src/utils/model.py
+++ src/utils/model.py
@@ -6,7 +6,7 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
     LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="inputLayer"),
           tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer1"),
-          tf.keras.layers.Dense(100, activation="relu", name="hiddenLayer2"),
+          tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer2"),
           tf.keras.layers.Dense(NUM_CLASSES, activation="softmax", name="outputLayer")]
 
     model_clf = tf.keras.models.Sequential(LAYERS)
